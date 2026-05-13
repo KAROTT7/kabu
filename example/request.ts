de7aec0238ad384f7e72d33deb1cec4892a04a08
shuzhiqiang <shuzhiqiang@local>
2026-05-13 09:36:04 +0800

    chore: 删除多余的代码

--- example/request.ts
+++ example/request.ts
@@ -1,19 +1,10 @@
-import axios, { type AxiosRequestConfig } from 'axios'
+import axios from 'axios'
 
-export interface RequestInstance {
-  get<T = unknown, R = T, D = unknown>(url: string, config?: AxiosRequestConfig<D>): Promise<R>
-  delete<T = unknown, R = T, D = unknown>(url: string, config?: AxiosRequestConfig<D>): Promise<R>
-  post<T = unknown, R = T, D = unknown>(url: string, data?: D, config?: AxiosRequestConfig<D>): Promise<R>
-  put<T = unknown, R = T, D = unknown>(url: string, data?: D, config?: AxiosRequestConfig<D>): Promise<R>
-  patch<T = unknown, R = T, D = unknown>(url: string, data?: D, config?: AxiosRequestConfig<D>): Promise<R>
-}
 
-const axiosRequest = axios.create({
+const request = axios.create({
   timeout: 10000
 })
 
-axiosRequest.interceptors.response.use(response => response.data)
-
-const request = axiosRequest as RequestInstance
+request.interceptors.response.use(response => response.data)
 
 export default request
